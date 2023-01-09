b05049bdefed906c2b9075ae421f354933ce68e6
Marcelo Dias Avelino <valorcurse@gmail.com>
2023-01-09 09:39:43 +0100

    Add listener to onChange

--- keepwatching.js
+++ keepwatching.js
@@ -1,7 +1,7 @@
 // Disable debug logging
-console.debug = function () {};
+// console.debug = function () {};
 
-console.debug("KeepWatching: loaded!");
+console.debug("ResumeWatching: loaded!");
 const PLAYER_STATE = {
     PLAYING: 1,
     PAUSED: 2
@@ -10,28 +10,40 @@ const PLAYER_STATE = {
 let player = document.getElementById("movie_player").wrappedJSObject;
 let videoID = player.getVideoData()['video_id'];
 
+
 let storedCurrentTime = browser.storage.sync.get(videoID).then(function onGot(item) {
     if (item.hasOwnProperty(videoID)) {
         item = item[videoID];
         if (item.hasOwnProperty("currentTime")) {
-            console.debug("KeepWatching:", item["currentTime"]);
+            console.debug("ResumeWatching:", "Found stored time for this video", item["currentTime"]);
             player.seekTo(item["currentTime"]);        
         } 
     } else {
-        console.debug("KeepWatching:", "Couldn't find stored time for this video.");
+        console.debug("ResumeWatching:", "Couldn't find stored time for this video.");
     }
     
     poll();
 });
 
+browser.storage.sync.onChanged.addListener((changes) => {
+
+    let currentTime = Math.floor(player.getCurrentTime());
+    if (changes.hasOwnProperty(videoID)) {
+        let newTime = Math.floor(changes[videoID].newValue.currentTime);
+        
+        if (newTime != currentTime) {
+            player.seekTo(newTime);      
+        }  
+    }
+});
 async function poll() {
     
     while (true) {
         if (player.getPlayerState() === PLAYER_STATE.PLAYING) {
             
-            let currentTime = player.getCurrentTime();
+            let currentTime = Math.floor(player.getCurrentTime());
             
-            console.debug("KeepWatching: ", "Storing time", currentTime);
+            console.debug("ResumeWatching:", "Storing time", currentTime);
             browser.storage.sync.set({
                 [videoID]: { currentTime: currentTime }
             });
